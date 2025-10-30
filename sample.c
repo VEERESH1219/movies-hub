86a6684903b6dd6481b9fe3f3f8e8eb20cdf87db
MUNJULURI VEERESHKUMAR <veereshmunjuluri1219@gmail.com>
2025-10-30 19:48:13 +0530

    new files

---
+++ sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    FILE *fp; 
+    fp = fopen("notes.txt", "r"); 
+
+    fprintf(fp, "Veeresh\n");
+    fprintf(fp, "Pavan\n");
+    fprintf(fp, "Sandeep\n");
+    fprintf(fp, "Vamsi\n");
+
+    fclose(fp); 
+    printf("File written and closed successfully.\n");
+
+    return 0;
+}
